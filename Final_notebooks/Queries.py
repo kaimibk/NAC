8754054f9b60a64d65bc0db9aac53b8a6b57069a
kaimibk <kaimibk@hawaii.edu>
2018-07-14 18:44:56 -0400

    Debugging

--- Final_notebooks/Queries.py
+++ Final_notebooks/Queries.py
@@ -5,15 +5,16 @@ from astroquery.gaia import Gaia;
 
 search_radius = 5*u.arcsecond
 
+
 def query_survey(dataframe, idx, features, survey):
 	series = dataframe.loc[idx]
-	coord = SkyCoord(ra=series.ra, dec=series.dec)
+	coord = SkyCoord(ra=series.RA, dec=series.Dec, unit=(u.deg, u.deg))
 	try:
 		if survey == '2MASS':
-			temp = Irsa.query_region_async(coord, radius=search_radius, catalog='fp_psc').to_pandas()
+			temp = Irsa.query_region(coord, radius=search_radius, catalog='fp_psc').to_pandas()
 
 		elif survey == 'GAIA':
-			temp = Gaia.query_object_async(coord, radius=search_radius).to_pandas()
+			temp = Gaia.query_object(coordinate=coord, width=search_radius, height=search_radius).to_pandas()
 
 		else:
 			print('Invalid Survey')
@@ -23,7 +24,7 @@ def query_survey(dataframe, idx, features, survey):
 		i, _, _ = match_coordinates_sky(coord, catalog)
 
 		for feature in features:
-			series.at[idx, feature] = temp.at[int(i), feature]
+			dataframe.at[idx, feature] = temp.at[int(i), feature]
 
 	except Exception as e:
 		print(e)
